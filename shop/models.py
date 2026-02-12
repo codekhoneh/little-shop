50d8348339e187cb089200dc4b9a7f341aa9a938
Reza Dehghan <dehghanreza525@gmail.com>
2026-02-12 19:09:32 +0330

    Completing our collection section

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,56 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+class Product(models.Model):
+    name=models.CharField(
+        max_length=100,
+        verbose_name="نام محصول"
+    )
+
+    def __str__(self):
+        return self.name
+
+class Collection(models.Model):
+    title_main = models.CharField(
+        max_length=100,
+        verbose_name="عنوان اصلی"
+    )
+    title_sub = models.CharField(
+        max_length=150,
+        verbose_name="عنوان فرعی"
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True
+    )
+
+    image = models.ImageField(
+        upload_to="collections/",
+        blank=True,
+        null=True
+    )
+
+    products = models.ManyToManyField(
+        "Product",
+        related_name="collections",
+        blank=True
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.title_main}-{self.title_sub}")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title_main} - {self.title_sub}"
+
