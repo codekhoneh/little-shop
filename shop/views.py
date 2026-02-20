7b659b32fca65e1dc0d7e7977acae303a932eb94
Reza Dehghan <dehghanreza525@gmail.com>
2026-02-20 18:31:41 +0330

    refactor: extract sorting logic and create reusable sort partial

--- shop/views.py
+++ shop/views.py
@@ -8,6 +8,7 @@ from django.core.paginator import Paginator
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .utils import apply_sort
 
 
 
@@ -15,35 +16,29 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class CollectionDetailView(DetailView):
     model = Collection
     template_name = 'shop/collection.html'
-    context_object_name = 'collection'
     slug_field = 'slug'
-    slug_url_kwarg = 'slug' 
-    
+    slug_url_kwarg = 'slug'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        products = self.object.products.filter(is_active=True,variants__stock__gt=0).distinct()
+        products = self.object.products.filter(
+            is_active=True,
+            variants__stock__gt=0
+        ).distinct()
+
         sort = self.request.GET.get("sort")
 
-        if sort == "price_asc":
-            products = products.order_by("price")
+        # ✅ فقط sort از utility میاد
+        products = apply_sort(products, sort)
 
-        elif sort == "price_desc":
-            products = products.order_by("-price")
-
-        elif sort == "newest":
-            products = products.order_by("-id")  # فعلاً چون created_at در Product نداریم
-
-        else:
-            # پیش‌فرض → جدیدترین
-            products = products.order_by("-id")
-
-        paginator = Paginator(products, 1) 
+        # ✅ pagination هنوز اینجاست (داخل view)
+        paginator = Paginator(products, 6)
         page_number = self.request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
-        context["page_obj"] = page_obj
         context["products"] = page_obj
+        context["page_obj"] = page_obj
         context["sort"] = sort
 
         return context
